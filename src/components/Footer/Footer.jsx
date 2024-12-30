795f9379f2acbfd36ce0e421e669e3c5bc29837c
Nishant Bansal <bansalnishant3516@gmail.com>
2024-12-30 13:34:53 +0530

    Updated Service Page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,18 +19,24 @@ const Footer = () => {
                 <div className="footer-column">
                     <h4>Quick Links</h4>
                     <ul>
-                        <li><a href="home">Home</a></li>
+                        <li><a href="/">Home</a></li>
                         <li><a href="about">About Us</a></li>
                         <li><a href="services">Services</a></li>
                         <li><a href="contact">Contact</a></li>
+                    </ul>
+                    <h4>Follow Us</h4>
+                    <ul>
+                        <li><a href="https://www.facebook.com/expression.facility.management.services?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-facebook-f"></i> 
+                        </a></li>
                     </ul>
                 </div>
 
                 <div className="footer-column">
                     <h3>Contacts</h3>
-                    <p>Address: Siliguri, West Bengal</p>
+                    <p>Address: Matigara, Siliguri, West Bengal - 734003</p>
                     <h4>Email-Us</h4>
-                    <p>Email: info@expressionfacility.com</p>
+                    <p>Email: expressionfacility2023@gmail.com</p>
                     <h4>Call Now</h4>
                     <p>Phone: +91 96350 67373</p>
                 </div>
--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import './ServicePage.css'; 
-import housekeepingHeroImage from '../../assets/services/house_kepping_hero_image.png'
-import lanudryHeroImage from '../../assets/services/laundry.jpg'
-import bioMedicalHeroImage from "../../assets/services/biomedical_hero_image.png"
-import pestHeroImage from '../../assets/services/pest_hero_image.png'
-import securityHeroImage from '../../assets/services/security_hero_image.jpg'
-import wardboyHeroImage from '../../assets/services/ward_boy_hero_image.png'
-import SectorsSection from "../../components/SectorsSection/SectorSection"
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./ServicePage.css";
+import housekeepingHeroImage from "../../assets/services/house_kepping_hero_image.png";
+import lanudryHeroImage from "../../assets/services/laundry_hero_image.png";
+import bioMedicalHeroImage from "../../assets/services/biomedical_hero_image.png";
+import pestHeroImage from "../../assets/services/pest_hero_image.png";
+import securityHeroImage from "../../assets/services/security_hero_image.png";
+import wardboyHeroImage from "../../assets/services/ward_boy_hero_image.png";
+import SectorsSection from "../../components/SectorsSection/SectorSection";
 import healthImage from "../../assets/sectors/health.jpg";
 import buildingImage from "../../assets/sectors/building.jpg";
 import factoryImage from "../../assets/sectors/factory.jpg";
@@ -16,15 +16,38 @@ import officeImage from "../../assets/sectors/office.jpg";
 import resortImage from "../../assets/sectors/resort.jpg";
 import shopImage from "../../assets/sectors/shop.jpg";
 import shoppingImage from "../../assets/sectors/shopping.jpg";
-import CTAButton from '../../components/CTAButton/CTAButton';
-import ServiceCarousel from "../../components/ServiceCarousel/ServiceCarousel"
+import CTAButton from "../../components/CTAButton/CTAButton";
+import ServiceCarousel from "../../components/ServiceCarousel/ServiceCarousel";
 
 const ServicePage = () => {
-  const { serviceId } = useParams(); 
+  const { serviceId } = useParams();
 
   const serviceData = [
-    { id: 1, title: 'House Keeping Services', description: "Our professional housekeeping services ensure a clean, safe, and comfortable environment for your healthcare facility. With a focus on hygiene and patient care, we provide thorough cleaning of all areas, including patient rooms, common spaces, and medical equipment. Our experienced team follows strict protocols to meet industry standards, ensuring a spotless and sanitized environment. Whether it's daily upkeep or deep cleaning, our services contribute to a healthier and more pleasant atmosphere for both patients and staff. Trust us to maintain your facility's cleanliness with reliability and efficiency.",  serviceList : [
-      { title: "Healthcare", icon: healthImage },
+    {
+      id: 1,
+      title: "House Keeping Services",
+      description:
+        "Our professional housekeeping services ensure a clean, safe, and comfortable environment for your healthcare facility. With a focus on hygiene and patient care, we provide thorough cleaning of all areas, including patient rooms, common spaces, and medical equipment. Our experienced team follows strict protocols to meet industry standards, ensuring a spotless and sanitized environment. Whether it's daily upkeep or deep cleaning, our services contribute to a healthier and more pleasant atmosphere for both patients and staff. Trust us to maintain your facility's cleanliness with reliability and efficiency.",
+      serviceList: [
+        { title: "Healthcare", icon: healthImage },
+        { title: "Hotels & Restaurants", icon: hotelImage },
+        { title: "Resorts", icon: resortImage },
+        { title: "Home/Office", icon: officeImage },
+      ],
+      backgroundImage: housekeepingHeroImage,
+      features: {
+        "fas fa-cogs": "Skilled",
+        "fas fa-hand-sparkles": "Adhere to Hygiene",
+        "fas fa-user-shield": "Avoid Invading Personal Space",
+      },
+    },
+    {
+      id: 2,
+      title: "Security Services",
+      description:
+        "Our top-tier security services are designed to provide a safe and secure environment for your healthcare facility. With highly trained security personnel, we offer 24/7 surveillance, access control, and emergency response. We prioritize patient and staff safety by implementing customized security plans that align with your facility's needs. Our team ensures the protection of both property and personnel while maintaining a welcoming atmosphere. Rely on our expert security services to safeguard your healthcare environment with professionalism and discretion.",
+      serviceList: [
+        { title: "Healthcare", icon: healthImage },
         { title: "Hotels & Restaurants", icon: hotelImage },
         { title: "Resorts", icon: resortImage },
         { title: "Malls", icon: shoppingImage },
@@ -32,26 +55,21 @@ const ServicePage = () => {
         { title: "Home/Office", icon: officeImage },
         { title: "Factory", icon: factoryImage },
         { title: "Construction Building", icon: buildingImage },
-    ], backgroundImage: housekeepingHeroImage},
-    { id: 2, title: 'Security Services', description: "Our top-tier security services are designed to provide a safe and secure environment for your healthcare facility. With highly trained security personnel, we offer 24/7 surveillance, access control, and emergency response. We prioritize patient and staff safety by implementing customized security plans that align with your facility's needs. Our team ensures the protection of both property and personnel while maintaining a welcoming atmosphere. Rely on our expert security services to safeguard your healthcare environment with professionalism and discretion.", serviceList : [
-      { title: "Healthcare", icon: healthImage },
-      { title: "Hotels & Restaurants", icon: hotelImage },
-      { title: "Resorts", icon: resortImage },
-      { title: "Malls", icon: shoppingImage },
-      { title: "Shop", icon: shopImage },
-      { title: "Home/Office", icon: officeImage },
-      { title: "Factory", icon: factoryImage },
-    ],backgroundImage: securityHeroImage, features: {"":"Skilled", "":"Adhere to Hygiene", "":"Avoid Invading Personal Space"}},
-    { id: 3, title: 'Pest Control Services', description: "Our professional pest control services ensure a hygienic and pest-free environment for your healthcare facility. We use safe, effective treatments to eliminate common pests while prioritizing the health and safety of patients and staff. Our team is trained in handling sensitive environments, offering discreet and timely pest management solutions. Whether it's for routine inspections or emergency treatments, we tailor our services to meet your needs. Trust us to maintain a clean and pest-free healthcare setting with our reliable, eco-friendly pest control services.", serviceList : [
-      { title: "Healthcare", icon: healthImage },
-        { title: "Hotels & Restaurants", icon: hotelImage },
-        { title: "Resorts", icon: resortImage },
-        { title: "Malls", icon: shoppingImage },
-        { title: "Shop", icon: shopImage },
-        { title: "Factory", icon: factoryImage },
-    ], backgroundImage: pestHeroImage },
-    { id: 4, title: 'Linen & Laundry Services', description: "Our linen and laundry services offer reliable, high-quality solutions for healthcare facilities. We provide clean, sanitized linens, including bed sheets, towels, and patient gowns, ensuring a hygienic environment for both patients and staff. Using advanced cleaning methods and eco-friendly detergents, we guarantee the highest standards of cleanliness and fabric care. Our flexible services cater to your facility's needs, offering timely deliveries and pick-ups. Trust us to handle your linen with care, providing fresh, sterile, and well-maintained laundry for a safe and comfortable healthcare experience.", serviceList : [
-      { title: "Healthcare", icon: healthImage },
+      ],
+      backgroundImage: securityHeroImage,
+      features: {
+        "fas fa-cogs": "Tailored Security Solutions",
+        "fas fa-headset": "24/7 Emergency Assistance",
+        "fas fa-tools": "Advanced Equipment",
+      },
+    },
+    {
+      id: 3,
+      title: "Pest Control Services",
+      description:
+        "Our professional pest control services ensure a hygienic and pest-free environment for your healthcare facility. We use safe, effective treatments to eliminate common pests while prioritizing the health and safety of patients and staff. Our team is trained in handling sensitive environments, offering discreet and timely pest management solutions. Whether it's for routine inspections or emergency treatments, we tailor our services to meet your needs. Trust us to maintain a clean and pest-free healthcare setting with our reliable, eco-friendly pest control services.",
+      serviceList: [
+        { title: "Healthcare", icon: healthImage },
         { title: "Hotels & Restaurants", icon: hotelImage },
         { title: "Resorts", icon: resortImage },
         { title: "Malls", icon: shoppingImage },
@@ -59,9 +77,40 @@ const ServicePage = () => {
         { title: "Home/Office", icon: officeImage },
         { title: "Factory", icon: factoryImage },
         { title: "Construction Building", icon: buildingImage },
-    ], backgroundImage: lanudryHeroImage },
-    { id: 5, title: 'Bio Medical Waste', description: "Our biomedical waste management services ensure safe, compliant disposal of medical waste, protecting both patients and the environment. We follow strict guidelines and regulations to handle, segregate, and dispose of hazardous materials such as sharps, used medical equipment, and contaminated items. Our team provides secure, efficient collection and transportation of waste, ensuring proper treatment and disposal. With a focus on safety and environmental responsibility, we help healthcare facilities maintain a clean, risk-free environment. Trust us to manage your biomedical waste with professionalism and adherence to regulatory standards.", serviceList : [
-      { title: "Healthcare", icon: healthImage },
+      ],
+      backgroundImage: pestHeroImage,
+      features: {
+        "fas fa-bug": "Ant Control",
+        "fas fa-bed": "Bed Bug Removal",
+        "fas fa-paw": "Rodent Control",
+        "fas fa-paw": "Rats Control",
+      },
+    },
+    {
+      id: 4,
+      title: "Linen & Laundry Services",
+      description:
+        "Our linen and laundry services offer reliable, high-quality solutions for healthcare facilities. We provide clean, sanitized linens, including bed sheets, towels, and patient gowns, ensuring a hygienic environment for both patients and staff. Using advanced cleaning methods and eco-friendly detergents, we guarantee the highest standards of cleanliness and fabric care. Our flexible services cater to your facility's needs, offering timely deliveries and pick-ups. Trust us to handle your linen with care, providing fresh, sterile, and well-maintained laundry for a safe and comfortable healthcare experience.",
+      serviceList: [
+        { title: "Healthcare", icon: healthImage },
+        { title: "Hotels & Restaurants", icon: hotelImage },
+        { title: "Resorts", icon: resortImage },
+        { title: "Factory", icon: factoryImage },
+      ],
+      backgroundImage: lanudryHeroImage,
+      features: {
+        "fas fa-cogs": "Systematic Process",
+        "fas fa-sync-alt": "Multiple Disinfections",
+        "fas fa-shield-alt": "Safe & Secure Chemical usage",
+      },
+    },
+    {
+      id: 5,
+      title: "Bio Medical Waste",
+      description:
+        "Our biomedical waste management services ensure safe, compliant disposal of medical waste, protecting both patients and the environment. We follow strict guidelines and regulations to handle, segregate, and dispose of hazardous materials such as sharps, used medical equipment, and contaminated items. Our team provides secure, efficient collection and transportation of waste, ensuring proper treatment and disposal. With a focus on safety and environmental responsibility, we help healthcare facilities maintain a clean, risk-free environment. Trust us to manage your biomedical waste with professionalism and adherence to regulatory standards.",
+      serviceList: [
+        { title: "Healthcare", icon: healthImage },
         { title: "Hotels & Restaurants", icon: hotelImage },
         { title: "Resorts", icon: resortImage },
         { title: "Malls", icon: shoppingImage },
@@ -69,27 +118,38 @@ const ServicePage = () => {
         { title: "Home/Office", icon: officeImage },
         { title: "Factory", icon: factoryImage },
         { title: "Construction Building", icon: buildingImage },
-    ], backgroundImage: bioMedicalHeroImage },
-    { id: 6, title: 'Ward Boy Services', description: "Our ward boy services provide essential support to healthcare facilities by ensuring smooth operations and enhancing patient care. Our trained staff assist with daily tasks such as patient mobility, delivering meals, maintaining cleanliness in patient rooms, and offering general assistance to healthcare teams. With a focus on compassion and efficiency, our ward boys contribute to a comfortable and well-organized environment for patients and staff. Rely on our dedicated professionals to provide reliable support, allowing your medical team to focus on delivering top-quality healthcare.", serviceList : [
-      { title: "Healthcare", icon: healthImage },
-        { title: "Hotels & Restaurants", icon: hotelImage },
-        { title: "Resorts", icon: resortImage },
-        { title: "Malls", icon: shoppingImage },
-        { title: "Shop", icon: shopImage },
-        { title: "Home/Office", icon: officeImage },
-        { title: "Factory", icon: factoryImage },
-        { title: "Construction Building", icon: buildingImage },
-    ], backgroundImage: wardboyHeroImage },
+      ],
+      backgroundImage: bioMedicalHeroImage,
+      features: {
+        "fas fa-trash-alt": "Waste Collection",
+        "fas fa-biohazard": "Biohazard Waste",
+        // "fas fa-mask": "Infection Control",
+        "fas fa-recycle": "Waste Disposal",
+      },
+    },
+    {
+      id: 6,
+      title: "Ward Boy Services",
+      description:
+        "Our ward boy services provide essential support to healthcare facilities by ensuring smooth operations and enhancing patient care. Our trained staff assist with daily tasks such as patient mobility, delivering meals, maintaining cleanliness in patient rooms, and offering general assistance to healthcare teams. With a focus on compassion and efficiency, our ward boys contribute to a comfortable and well-organized environment for patients and staff. Rely on our dedicated professionals to provide reliable support, allowing your medical team to focus on delivering top-quality healthcare.",
+      serviceList: [{ title: "Healthcare", icon: healthImage }],
+      backgroundImage: wardboyHeroImage,
+      features: {
+        "fas fa-user-tie": "Skilled",
+        "fas fa-hand-sparkles": "Adhere to Hygiene",
+        "fas fa-users-slash": "Avoid Invading Personal Space",
+      },
+    },
   ];
 
-  const service = serviceData.find(s => s.id === parseInt(serviceId)); 
+  const service = serviceData.find((s) => s.id === parseInt(serviceId));
 
   if (!service) {
     return <p>Service not found</p>;
   }
 
-const serviceList = [
-  { title: "Healthcare", icon: healthImage },
+  const serviceList = [
+    { title: "Healthcare", icon: healthImage },
     { title: "Hotels & Restaurants", icon: hotelImage },
     { title: "Resorts", icon: resortImage },
     { title: "Malls", icon: shoppingImage },
@@ -97,53 +157,41 @@ const serviceList = [
     { title: "Home/Office", icon: officeImage },
     { title: "Factory", icon: factoryImage },
     { title: "Construction Building", icon: buildingImage },
-]
-  
+  ];
+
   return (
     <div className="service-page-container">
       <div
         className="service-page-image"
         style={{
           backgroundImage: `url(${service.backgroundImage})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          height: '80vh',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "80vh",
         }}
-      >
-      </div>
+      ></div>
       <div className="service-page-content">
-      <h1>About Service</h1>
+        <h1>About Service</h1>
         <p>{service.description}</p>
       </div>
-        <div className="about-us-content">
-        <h2>{service.title.replace(" Services","")} Features</h2>
+      <div className="about-us-content">
+        <h2>{service.title.replace(" Services", "")} Features</h2>
         <div className="values-list">
-          <div className="value-item">
-            <div className="value-icon">
-              <i className="fas fa-users"></i>
-            </div>
-            <h3>Customer-Focused Success</h3>
-          </div>
-
-          <div className="value-item">
-            <div className="value-icon">
-              <i className="fas fa-cogs"></i>
-            </div>
-            <h3>Solution-Oriented Approach</h3>
-          </div>
-
-          <div className="value-item">
-            <div className="value-icon">
-              <i className="fas fa-heart"></i>
-            </div>
-            <h3>Careful and Considerate Actions</h3>
-          </div>
+          {Object.entries(service.features).map(
+            ([iconClass, description], index) => (
+              <div key={index} className="value-item">
+                <div className="value-icon">
+                  <i className={iconClass}></i>
+                </div>
+                <h3>{description}</h3>
+              </div>
+            )
+          )}
         </div>
       </div>
       <SectorsSection list={serviceList} />
       {/* <ServiceCarousel/> */}
-      <CTAButton/>
-
+      <CTAButton />
     </div>
   );
 };
