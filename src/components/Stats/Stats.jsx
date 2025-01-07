d99bb51db7722310bc30ee54e54ba1b7251acf20
Nishant Bansal <bansalnishant3516@gmail.com>
2025-01-07 21:36:16 +0530

    Fixed the Sector Section mimimize issue

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import './Stats.css';
+import React from "react";
+import "./Stats.css";
 import tempLogo from "../../assets/expression_without_bg_logo.png";
 
 const MedicalAchievementHealthProtection = () => {
   return (
     <div className="container">
       <div className="header-content">
-        <h1>Global <b style={{ fontSize: 48 }}>Standards</b>,<br />At <b style={{ fontSize: 48 }}>Local Care</b></h1>
+        <h1>
+          Global <b style={{ fontSize: 48 }}>Standards</b>,<br />
+          At <b style={{ fontSize: 48 }}>Local Care</b>
+        </h1>
         <div className="stats-container">
           <div className="stat">
             <span className="stat-value">15+</span>
-            <span className="stat-label">Satisfied Patients</span>
+            <span className="stat-label">Satisfied Clients</span>
           </div>
           <div className="stat">
             <span className="stat-value">90%</span>
             <span className="stat-label">Satisfaction Rates</span>
           </div>
           <div className="stat">
-            <span className="stat-value">2</span>
+            <span className="stat-value">2+</span>
             <span className="stat-label">Working States</span>
-            <p>Bihar, West Bengal</p>
+            {/* <p>Bihar, West Bengal</p> */}
           </div>
         </div>
       </div>
